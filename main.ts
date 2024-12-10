a3e272db94f941edcc31cad97add2fbd69215826
Zhiqin-Wei <zhiqin.daly.wei@mths.ca>
2024-12-09 20:21:27 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Daly Wei
+ * Created on: oct 2024
+ * This program show LED 
 */
 
-basic.showString('Hello, World!')
+
+// Happy face at start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+let redPin = DigitalPin.P13
+let greenPin = DigitalPin.P14
+let bluePin = DigitalPin.P15
+
+input.onButtonPressed(Button.A, function() {
+    pins.digitalWritePin(redPin, 1)
+    pins.digitalWritePin(greenPin, 0)
+    pins.digitalWritePin(bluePin, 0)
+    basic.pause(1000)
+
+    pins.digitalWritePin(redPin, 0)
+    pins.digitalWritePin(greenPin, 1)
+    pins.digitalWritePin(bluePin, 0)
+    basic.pause(1000)
+
+    pins.digitalWritePin(redPin, 0)
+    pins.digitalWritePin(greenPin, 0)
+    pins.digitalWritePin(bluePin, 1)
+    basic.pause(1000)
+
+    pins.digitalWritePin(redPin, 0)
+    pins.digitalWritePin(greenPin, 0)
+    pins.digitalWritePin(bluePin, 0)
+    basic.pause(1000)
+})
